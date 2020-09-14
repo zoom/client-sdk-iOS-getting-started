1b3b1db3e852c3138f948146df4431bc058611ae
Michael Condon <superialcondon@gmail.com>
2020-09-14 15:20:59 -0600

    1.0 (#1)
    * Version 1.0

---
+++ ObjectiveCDemo/ZoomiOSSDKDemoInObjC/ViewController.m
@@ -0,0 +1,215 @@
+//
+//  ViewController.m
+//  ZoomiOSSDKDemoInObjC
+//
+//  Created by Zoom Video Communications on 8/18/20.
+//  Copyright © 2020 Zoom Video Communications. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <MobileRTC/MobileRTC.h>
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+#pragma mark - LifeCycle
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    UINavigationController *navController = self.navigationController;
+
+    // The Zoom SDK requires a UINavigationController to update the UI for us. Here we supplied the SDK with the ViewControllers navigationController.
+    if (navController)
+        [[MobileRTC sharedRTC] setMobileRTCRootController:navController];
+
+    /// Notification that is used to start a meeting upon log in success.
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(userLoggedIn) name:@"userLoggedIn" object:nil];
+}
+
+
+#pragma mark - IBOutlets
+
+
+- (IBAction)joinAMeetingButtonPressed:(UIButton *)sender {
+    [self presentJoinMeetingAlert];
+}
+
+
+- (IBAction)startAnInstantMeetingButtonPressed:(UIButton *)sender {
+    // Obtain the MobileRTCAuthService from the Zoom SDK.
+    MobileRTCAuthService *authService = [[MobileRTC sharedRTC] getAuthService];
+
+    if (authService) {
+        // Check if the user is logged into Zoom.
+        BOOL isLoggedIn = [authService isLoggedIn];
+        if (isLoggedIn) {
+            [self startMeeting];
+        } else {
+            [self presentLogInAlert];
+        }
+    }
+}
+
+#pragma mark - Zoom SDK Examples
+
+
+- (void)joinMeeting:(NSString *)meetingNumber meetingPassword:(NSString *)meetingPassword {
+    // Obtain the MobileRTCMeetingService from the Zoom SDK, this service can start meetings, join meetings, leave meetings, etc.
+    MobileRTCMeetingService *meetService = [[MobileRTC sharedRTC] getMeetingService];
+
+    if (meetService) {
+        // Set the ViewContoller to be the MobileRTCMeetingServiceDelegate
+        meetService.delegate = self;
+
+        // Create a MobileRTCMeetingJoinParam to provide the MobileRTCMeetingService with the necessary info to join a meeting.
+        // In this case, we will only need to provide a meeting number and password.
+        MobileRTCMeetingJoinParam *joinParams = [[MobileRTCMeetingJoinParam alloc] init];
+        joinParams.meetingNumber = meetingNumber;
+        joinParams.password = meetingPassword;
+
+        // Call the joinMeeting function in MobileRTCMeetingService. The Zoom SDK will handle the UI for you, unless told otherwise.
+        // If the meeting number and meeting password are valid, the user will be put into the meeting. A waiting room UI will be presented or the meeting UI will be presented.
+        [meetService joinMeetingWithJoinParam:joinParams];
+    }
+}
+
+
+- (void)logIn:(NSString *)email password:(NSString *)password {
+    // Obtain the MobileRTCAuthService from the Zoom SDK, this service can log in a Zoom user, log out a Zoom user, authorize the Zoom SDK etc.
+    MobileRTCAuthService *authService = [[MobileRTC sharedRTC] getAuthService];
+
+    if (authService)
+        // Call the login function in MobileRTCAuthService. This will attempt to log in the user.
+        [authService loginWithEmail:email password:password rememberMe:false];
+}
+
+
+- (void)startMeeting {
+    // Obtain the MobileRTCMeetingService from the Zoom SDK, this service can start meetings, join meetings, leave meetings, etc.
+    MobileRTCMeetingService *meetService = [[MobileRTC sharedRTC] getMeetingService];
+
+    if (meetService) {
+        // Set the ViewContoller to be the MobileRTCMeetingServiceDelegate
+        meetService.delegate = self;
+
+        // Create a MobileRTCMeetingStartParam to provide the MobileRTCMeetingService with the necessary info to start an instant meeting.
+        // In this case we will use MobileRTCMeetingStartParam4LoginlUser, since the user has logged into Zoom.
+        MobileRTCMeetingStartParam * startParams = [[MobileRTCMeetingStartParam4LoginlUser alloc] init];
+
+        // Call the startMeeting function in MobileRTCMeetingService. The Zoom SDK will handle the UI for you, unless told otherwise.
+        [meetService startMeetingWithStartParam:startParams];
+    }
+}
+
+
+#pragma mark - Convenience Alerts
+
+
+/// Creates alert for prompting the user to enter meeting number and password for joining a meeting.
+- (void)presentJoinMeetingAlert {
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Join meeting" message:@"" preferredStyle:UIAlertControllerStyleAlert];
+
+    [alertController addTextFieldWithConfigurationHandler:^(UITextField * _Nonnull textField) {
+        textField.placeholder = @"Meeting number";
+        textField.keyboardType = UIKeyboardTypeNumberPad;
+    }];
+    [alertController addTextFieldWithConfigurationHandler:^(UITextField * _Nonnull textField) {
+        textField.placeholder = @"Meeting password";
+        textField.keyboardType = UIKeyboardTypeASCIICapable;
+        textField.secureTextEntry = YES;
+    }];
+
+    UIAlertAction *joinMeetingAction = [UIAlertAction actionWithTitle:@"Join meeting" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        NSString *number = [[alertController textFields][0] text];
+        NSString *password = [[alertController textFields][1] text];
+
+        if (number && password)
+            [self joinMeeting:number meetingPassword:password];
+
+    }];
+
+    [alertController addAction:joinMeetingAction];
+
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+        NULL;
+    }];
+
+    [alertController addAction:cancelAction];
+    [self presentViewController:alertController animated:YES completion:nil];
+}
+
+
+/// Creates alert for prompting the user to enter thier Zoom credentials for starting a meeting.
+- (void)presentLogInAlert {
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Log in" message:@"" preferredStyle:UIAlertControllerStyleAlert];
+
+    [alertController addTextFieldWithConfigurationHandler:^(UITextField * _Nonnull textField) {
+        textField.placeholder = @"Email";
+        textField.keyboardType = UIKeyboardTypeEmailAddress;
+    }];
+    [alertController addTextFieldWithConfigurationHandler:^(UITextField * _Nonnull textField) {
+        textField.placeholder = @"Password";
+        textField.keyboardType = UIKeyboardTypeASCIICapable;
+        textField.secureTextEntry = YES;
+    }];
+
+    UIAlertAction *logInAction = [UIAlertAction actionWithTitle:@"Log in" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        NSString *email = [[alertController textFields][0] text];
+        NSString *password = [[alertController textFields][1] text];
+
+        if (email && password)
+            [self logIn:email password:password];
+
+    }];
+
+    [alertController addAction:logInAction];
+
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+        NULL;
+    }];
+
+    [alertController addAction:cancelAction];
+    [self presentViewController:alertController animated:YES completion:nil];
+}
+
+
+#pragma mark - Internal
+
+/// Selector that is used to start a meeting upon log in success.
+- (void)userLoggedIn {
+    [self startMeeting];
+}
+
+
+#pragma mark - MobileRTCMeetingServiceDelegate
+
+// Conform ViewController to MobileRTCMeetingServiceDelegate.
+// MobileRTCMeetingServiceDelegate listens to updates about meetings, such as meeting state changes, join attempt status, meeting errors, etc.
+
+
+- (void)onMeetingError:(MobileRTCMeetError)error message:(NSString *)message {
+    switch (error) {
+        case MobileRTCMeetError_PasswordError:
+            NSLog(@"Could not join or start meeting because the meeting password was incorrect.");
+        default:
+            NSLog(@"Could not join or start meeting with MobileRTCMeetError: %u %@", error, message);
+    }
+}
+
+// Is called when the user joins a meeting.
+- (void)onJoinMeetingConfirmed {
+    NSLog(@"Join meeting confirmed.");
+}
+
+
+// Is called upon meeting state changes.
+- (void)onMeetingStateChange:(MobileRTCMeetingState)state {
+    NSLog(@"Current meeting state: %u", state);
+}
+
+
+@end
---
+++ SwiftDemo/ZoomiOSSDKDemo/ViewController.swift
@@ -0,0 +1,207 @@
+//
+//  ViewController.swift
+//  ZoomiOSSDKDemo
+//
+//  Created by Zoom Video Communications on 8/14/20.
+//  Copyright © 2020 Zoom Video Communications. All rights reserved.
+//
+
+import UIKit
+import MobileRTC
+
+class ViewController: UIViewController {
+
+    // MARK: - LifeCycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // The Zoom SDK requires a UINavigationController to update the UI for us. Here we supplied the SDK with the ViewControllers navigationController.
+        MobileRTC.shared().setMobileRTCRootController(self.navigationController)
+
+        /// Notification that is used to start a meeting upon log in success.
+        NotificationCenter.default.addObserver(self, selector: #selector(userLoggedIn), name: NSNotification.Name(rawValue: "userLoggedIn"), object: nil)
+    }
+
+    // MARK: - IBOutlets
+
+    @IBAction func joinAMeetingButtonPressed(_ sender: Any) {
+        presentJoinMeetingAlert()
+    }
+
+    @IBAction func startAnInstantMeetingButtonPressed(_ sender: Any) {
+        // Obtain the MobileRTCAuthService from the Zoom SDK, and check if the user is logged into Zoom.
+        if let authorizationService = MobileRTC.shared().getAuthService(), authorizationService.isLoggedIn() {
+            startMeeting()
+        } else {
+            presentLogInAlert()
+        }
+    }
+
+    // MARK: - Zoom SDK Examples
+
+    /// Puts user into ongoing Zoom meeting using a known meeting number and meeting password.
+    ///
+    /// Assign a MobileRTCMeetingServiceDelegate to listen to meeting events and join meeting status.
+    ///
+    /// - Parameters:
+    ///   - meetingNumber: The meeting number of the desired meeting.
+    ///   - meetingPassword: The meeting password of the desired meeting.
+    /// - Precondition:
+    ///   - Zoom SDK must be intitalized and authorized.
+    ///   - MobileRTC.shared().setMobileRTCRootController() has been called.
+    func joinMeeting(meetingNumber: String, meetingPassword: String) {
+        // Obtain the MobileRTCMeetingService from the Zoom SDK, this service can start meetings, join meetings, leave meetings, etc.
+        if let meetingService = MobileRTC.shared().getMeetingService() {
+
+            // Set the ViewContoller to be the MobileRTCMeetingServiceDelegate
+            meetingService.delegate = self
+
+            // Create a MobileRTCMeetingJoinParam to provide the MobileRTCMeetingService with the necessary info to join a meeting.
+            // In this case, we will only need to provide a meeting number and password.
+            let joinMeetingParameters = MobileRTCMeetingJoinParam()
+            joinMeetingParameters.meetingNumber = meetingNumber
+            joinMeetingParameters.password = meetingPassword
+
+            // Call the joinMeeting function in MobileRTCMeetingService. The Zoom SDK will handle the UI for you, unless told otherwise.
+            // If the meeting number and meeting password are valid, the user will be put into the meeting. A waiting room UI will be presented or the meeting UI will be presented.
+            meetingService.joinMeeting(with: joinMeetingParameters)
+        }
+    }
+
+    /// Logs user into their Zoom account using the user's Zoom email and password.
+    ///
+    /// Assign a MobileRTCAuthDelegate to listen to authorization events including onMobileRTCLoginReturn(_ returnValue: Int).
+    ///
+    /// - Parameters:
+    ///   - email: The user's email address attatched to their Zoom account.
+    ///   - password: The user's password attatched to their Zoom account.
+    /// - Precondition:
+    ///   - Zoom SDK must be intitalized and authorized.
+    func logIn(email: String, password: String) {
+        // Obtain the MobileRTCAuthService from the Zoom SDK, this service can log in a Zoom user, log out a Zoom user, authorize the Zoom SDK etc.
+        if let authorizationService = MobileRTC.shared().getAuthService() {
+            // Call the login function in MobileRTCAuthService. This will attempt to log in the user.
+            authorizationService.login(withEmail: email, password: password, rememberMe: false)
+        }
+    }
+
+    /// Creates and starts a Zoom instant meeting. An instant meeting is an unscheduled meeting that begins instantly.
+    ///
+    /// Assign a MobileRTCMeetingServiceDelegate to listen to meeting events and start meeting status.
+    ///
+    /// - Precondition:
+    ///   - Zoom SDK must be intitalized and authorized.
+    ///   - MobileRTC.shared().setMobileRTCRootController() has been called.
+    ///   - User has logged into Zoom successfully.
+    func startMeeting() {
+        // Obtain the MobileRTCMeetingService from the Zoom SDK, this service can start meetings, join meetings, leave meetings, etc.
+        if let meetingService = MobileRTC.shared().getMeetingService() {
+            // Set the ViewContoller to be the MobileRTCMeetingServiceDelegate
+            meetingService.delegate = self
+
+            // Create a MobileRTCMeetingStartParam to provide the MobileRTCMeetingService with the necessary info to start an instant meeting.
+            // In this case we will use MobileRTCMeetingStartParam4LoginlUser(), since the user has logged into Zoom.
+            let startMeetingParameters = MobileRTCMeetingStartParam4LoginlUser()
+
+            // Call the startMeeting function in MobileRTCMeetingService. The Zoom SDK will handle the UI for you, unless told otherwise.
+            meetingService.startMeeting(with: startMeetingParameters)
+        }
+    }
+
+    // MARK: - Convenience Alerts
+
+    /// Creates alert for prompting the user to enter meeting number and password for joining a meeting.
+    func presentJoinMeetingAlert() {
+        let alertController = UIAlertController(title: "Join meeting", message: "", preferredStyle: .alert)
+
+        alertController.addTextField { (textField : UITextField!) -> Void in
+            textField.placeholder = "Meeting number"
+            textField.keyboardType = .phonePad
+        }
+        alertController.addTextField { (textField : UITextField!) -> Void in
+            textField.placeholder = "Meeting password"
+            textField.keyboardType = .asciiCapable
+            textField.isSecureTextEntry = true
+        }
+
+        let joinMeetingAction = UIAlertAction(title: "Join meeting", style: .default, handler: { alert -> Void in
+            let numberTextField = alertController.textFields![0] as UITextField
+            let passwordTextField = alertController.textFields![1] as UITextField
+
+            if let meetingNumber = numberTextField.text, let password = passwordTextField.text {
+                self.joinMeeting(meetingNumber: meetingNumber, meetingPassword: password)
+            }
+        })
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: { (action : UIAlertAction!) -> Void in })
+
+        alertController.addAction(joinMeetingAction)
+        alertController.addAction(cancelAction)
+
+        self.present(alertController, animated: true, completion: nil)
+    }
+
+    /// Creates alert for prompting the user to enter thier Zoom credentials for starting a meeting.
+    func presentLogInAlert() {
+        let alertController = UIAlertController(title: "Log in", message: "", preferredStyle: .alert)
+
+        alertController.addTextField { (textField : UITextField!) -> Void in
+            textField.placeholder = "Email"
+            textField.keyboardType = .emailAddress
+        }
+        alertController.addTextField { (textField : UITextField!) -> Void in
+            textField.placeholder = "Password"
+            textField.keyboardType = .asciiCapable
+            textField.isSecureTextEntry = true
+        }
+
+        let logInAction = UIAlertAction(title: "Log in", style: .default, handler: { alert -> Void in
+            let emailTextField = alertController.textFields![0] as UITextField
+            let passwordTextField = alertController.textFields![1] as UITextField
+
+            if let email = emailTextField.text, let password = passwordTextField.text {
+                self.logIn(email: email, password: password)
+            }
+        })
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: { (action : UIAlertAction!) -> Void in })
+
+        alertController.addAction(logInAction)
+        alertController.addAction(cancelAction)
+
+        self.present(alertController, animated: true, completion: nil)
+    }
+
+    // MARK: - Internal
+
+    /// Selector that is used to start a meeting upon log in success.
+    @objc func userLoggedIn() {
+        startMeeting()
+    }
+}
+
+// MARK: - MobileRTCMeetingServiceDelegate
+
+// Conform ViewController to MobileRTCMeetingServiceDelegate.
+// MobileRTCMeetingServiceDelegate listens to updates about meetings, such as meeting state changes, join attempt status, meeting errors, etc.
+extension ViewController: MobileRTCMeetingServiceDelegate {
+
+    // Is called upon in-meeting errors, join meeting errors, start meeting errors, meeting connection errors, etc.
+    func onMeetingError(_ error: MobileRTCMeetError, message: String?) {
+        switch error {
+        case MobileRTCMeetError_PasswordError:
+            print("Could not join or start meeting because the meeting password was incorrect.")
+        default:
+            print("Could not join or start meeting with MobileRTCMeetError: \(error) \(message ?? "")")
+        }
+    }
+
+    // Is called when the user joins a meeting.
+    func onJoinMeetingConfirmed() {
+        print("Join meeting confirmed.")
+    }
+
+    // Is called upon meeting state changes.
+    func onMeetingStateChange(_ state: MobileRTCMeetingState) {
+        print("Current meeting state: \(state)")
+    }
+}
