d4529f11f5c510d85c83d7dea4cd6c14b972b664
Michael Condon <superialcondon@gmail.com>
2020-11-25 13:10:10 -0700

    Updated ObjectiveC demo for v5.2.41735.0928 (#5)
    * Updated framework to v5.2.42037.1112
    
    Fixed typos, added appDelegate callbacks
    
    * Updated ObjectiveC demo for v5.2.41735.0928

--- ObjectiveCDemo/ZoomiOSSDKDemoInObjC/AppDelegate.m
+++ ObjectiveCDemo/ZoomiOSSDKDemoInObjC/AppDelegate.m
@@ -23,7 +23,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
     // Obtain your SDK Key and SDK Secret and paste it here.
-    // Your SDK Secret should NEVER be publically accessible, only use the sdk key and secret for testing this demo app.
+    // Your SDK Secret should NEVER be publicly accessible, only use the sdk key and secret for testing this demo app.
     // For your own application, you must obtain a JWT instead of using the SDK Key and SDK Secret.
     self.sdkKey = @"";
     self.sdkSecret = @"";
@@ -35,7 +35,7 @@
 
 
 //Logs the user out of the app upon application termination.
-// This is not a neccesary action. In real use cases, the SDK should be alerted of app events. For example, in - (void)applicationWillTerminate:(UIApplication *)application, [[MobileRTC sharedRTC] appWillTerminate] should be called.
+// This is not a necessary action. In real use cases, the SDK should be alerted of app events. For example, in - (void)applicationWillTerminate:(UIApplication *)application, [[MobileRTC sharedRTC] appWillTerminate] should be called.
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Obtain the MobileRTCAuthService from the Zoom SDK, this service can log in a Zoom user, log out a Zoom user, authorize the Zoom SDK etc.
     MobileRTCAuthService *authService = [[MobileRTC sharedRTC] getAuthService];
@@ -43,6 +43,27 @@
     // Call logoutRTC() to log the user out.
     if (authService)
         [authService logoutRTC];
+
+    // Notify MobileRTC of appWillTerminate call.
+    [[MobileRTC sharedRTC] appWillTerminate];
+}
+
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+    // Notify MobileRTC of appWillResignActive call.
+    [[MobileRTC sharedRTC] appWillResignActive];
+}
+
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    // Notify MobileRTC of appDidBecomeActive call.
+    [[MobileRTC sharedRTC] appDidBecomeActive];
+}
+
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    // Notify MobileRTC of appDidEnterBackgroud call.
+    [[MobileRTC sharedRTC] appDidEnterBackgroud];
 }
 
 
@@ -53,13 +74,11 @@
     context.domain = @"zoom.us";
     // Turns on SDK logging. This is optional.
     context.enableLog = YES;
-    // Sets the appGroupID. This value must be valid to be able to use screensharing.
-    context.appGroupId = @"";
 
     // Call initialize:(MobileRTCSDKInitContext *_Nonnull)context to create an instance of the Zoom SDK. Without initializing first, the SDK will not do anything. This call will return YES if the SDK was initialized successfully.
     BOOL sdkInitSuc = [[MobileRTC sharedRTC] initialize:context];
 
-    // Check if initializaiton was successful. Obtain a MobileRTCAuthService, this is for supplying credentials to the SDK for authorization.
+    // Check if initialization was successful. Obtain a MobileRTCAuthService, this is for supplying credentials to the SDK for authorization.
     if (sdkInitSuc) {
         MobileRTCAuthService *authService = [[MobileRTC sharedRTC] getAuthService];
 
@@ -91,7 +110,7 @@
             NSLog(@"SDK successfully initialized.");
             break;
         case MobileRTCAuthError_KeyOrSecretEmpty:
-            NSLog(@"SDK key/secret was not provided. Relpace sdkKey and sdkSecret at the top of this file with your SDK key/secret.");
+            NSLog(@"SDK key/secret was not provided. Replace sdkKey and sdkSecret at the top of this file with your SDK key/secret.");
             break;
         case MobileRTCAuthError_KeyOrSecretWrong:
             NSLog(@"SDK key/secret is not valid.");
@@ -100,7 +119,7 @@
             NSLog(@"SDK key/secret is not valid.");
             break;
         default:
-            NSLog(@"SDK Authorization failed with MobileRTCAuthError: %u", returnValue);
+            NSLog(@"SDK Authorization failed with MobileRTCAuthError: %lu", returnValue);
     }
 }
 
@@ -110,7 +129,7 @@
         case 0:
             NSLog(@"Successfully logged in");
 
-            // This alerts the ViewController that log in was successful. This is not a neccesary action.
+            // This alerts the ViewController that log in was successful. This is not a necessary action.
             [[NSNotificationCenter defaultCenter] postNotificationName:@"userLoggedIn" object:self];
             break;
         case 1002:
